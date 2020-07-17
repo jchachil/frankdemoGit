3a53ab076e90dd9f7a8e7ea78e271613ef85c417
Jamal CHACHIL <jamal.chachil@hotmail.com>
2020-07-17 11:14:39 +0200

    Ajoutez des fichiers projet.

---
+++ frankdemoGit/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace frankdemoGit.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
